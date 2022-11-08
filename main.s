3c8b7a2dbcc42baee501263b0ec07127af7ddd54
Laura-MacLeod <97027376+Laura-MacLeod@users.noreply.github.com>
2022-11-08 09:54:55 +0000

    Update main.s

--- main.s
+++ main.s
@@ -11,17 +11,25 @@ start:
 	movlw	0x0e
 	movwf	PORTE, A	    ; Set value to PORTE
 	
+	
+	movlw	0x03
+	movwf	PORTD		; Set clock pulse and output enable both om. PORTD to 0x03,
+				; for CP and OE on, PORTE=3, for CP on snd OE off, PORTE=2, for CP off and OE
+				; on, PORTE=1, for CP and OE both off, PORTE=0
+	
+				
 	movlw	0xff
 	movwf	0x02	    ; Counter of 0xff at position 0x02
-	
-	movlw	0xff
-	movwf	PORTD		; Set clock pulse PORTD to 0xff
 	
 	bra	delay
 	
 	
 delay:
+	decfsz	0x02
+	bra	delay
 	
+test:
+	movlw	0x05
 	
 	
 	
