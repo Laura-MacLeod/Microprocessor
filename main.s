8f58fc394cb195806377e34c0f862f7ab17561f0
Laura-MacLeod <97027376+Laura-MacLeod@users.noreply.github.com>
2022-11-17 12:27:52 +0000

    Update main.s

--- main.s
+++ main.s
@@ -2,15 +2,16 @@
 
 extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
 extrn	LCD_Setup, LCD_Write_Message, clear
-extrn	keypad_setup, keypad_read_row, keypad_read_column
+extrn	keypad_setup, keypad_read_row, keypad_read_column, combine, interpret
 	
 psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
-
+length:	    ds 1
     
 psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
-myArray:    ds 0x80 ; reserve 128 bytes for message data
+storage:    ds 0x80
+
 
 
 psect	code, abs	
@@ -19,11 +20,26 @@ rst: 	org 0x0
 
 	; ******* Programme FLASH read Setup Code ***********************
 setup:	call	keypad_setup
+	call	UART_Setup
+	call	LCD_Setup
+	movlw	0x0f
+	movwf	length
+	
     
 	; ******* Main programme ****************************************
 start: 	
 	call	keypad_read_row
+	call	delay
 	call	keypad_read_column
+	call	combine
+	call	interpret
+	lfsr	0, storage
+	movwf	POSTINC0
+	call	UART_Transmit_Message
+	movlw	length
+	addlw	0xff
+	lfsr	2, storage
+	call	LCD_Write_Message
 	bra	start
 loop: 	
 	
